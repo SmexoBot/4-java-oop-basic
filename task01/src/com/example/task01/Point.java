f8eb36514e400164028513bca4aa1237c8f62ba6
Smexo <larushkinevgeniy.ru@gmail.com>
2024-10-03 17:39:44 +0500

    Ларюшкин Евгений Задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,32 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+
+    }
+
+    public void flip(){
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    public double distance(Point point){
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append('(').append(x).append(", ").append(y).append(')');
+        return str.toString();
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
